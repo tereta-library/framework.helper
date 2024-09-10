59c75ce4b5b8e2f57c6e467f4be4178aa876b0da
Tereta Alexander <atereta@gomage.com>
2024-09-10 22:57:03 +0300

    Library bug fixing 0.0.62

--- src/ArrayManager.php
+++ src/ArrayManager.php
@@ -3,7 +3,23 @@
 namespace Framework\Helper;
 
 /**
- * @class Framework\Helper\ArrayConverter
+ * ·······································································
+ * : _____                        _                     _                :
+ * :|_   _|   ___   _ __    ___  | |_    __ _        __| |   ___  __   __:
+ * :  | |    / _ \ | '__|  / _ \ | __|  / _` |      / _` |  / _ \ \ \ / /:
+ * :  | |   |  __/ | |    |  __/ | |_  | (_| |  _  | (_| | |  __/  \ V / :
+ * :  |_|    \___| |_|     \___|  \__|  \__,_| (_)  \__,_|  \___|   \_/  :
+ * ·······································································
+ * ···························WWW.TERETA.DEV······························
+ * ·······································································
+ *
+ * @class Framework\Helper\ArrayManager
+ * @package Framework\Helper
+ * @link https://tereta.dev
+ * @since 2020-2024
+ * @license   http://www.apache.org/licenses/LICENSE-2.0  Apache License 2.0
+ * @author Tereta Alexander <tereta.alexander@gmail.com>
+ * @copyright 2020-2024 Tereta Alexander
  */
 class ArrayManager
 {
@@ -32,6 +48,12 @@ class ArrayManager
         return $array;
     }
 
+    /**
+     * @param string $inputKey
+     * @param array $array
+     * @param string $delimiter
+     * @return mixed
+     */
     public static function fetch(string $inputKey, array $array, string $delimiter = '.'): mixed
     {
         $keys = explode($delimiter, $inputKey);
@@ -53,11 +75,18 @@ class ArrayManager
         return $current;
     }
 
+    /**
+     * @param array $array1
+     * @param array $array2
+     * @return array
+     */
     public static function merge(array $array1, array $array2): array
     {
         foreach ($array2 as $key => $value) {
             if (is_array($value) && isset($array1[$key]) && is_array($array1[$key])) {
                 $array1[$key] = self::merge($array1[$key], $value);
+            } elseif (is_int($key)) {
+                $array1[] = $value;
             } else {
                 $array1[$key] = $value;
             }
